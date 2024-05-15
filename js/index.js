e9ade2e966932a5f87998a78651145302d850dbf
hguevara1 <hguevarasoto@gmail.com>
2024-05-15 18:53:00 -0400

    cambio de lenguaje según atributo

--- js/index.js
+++ js/index.js
@@ -1,15 +1,36 @@
-function iniciar(idPerfil) {
-    const filePath = "/reto5/"+ idPerfil +"/perfil.json";
+function iniciar(idPerfil, idioma) {
+    if (idPerfil === null) {
+        idPerfil = "10347014";
+    }
+    switch (idioma) {
+        case "Es":
+            idioma = "configES.json"
+            break;
+        case "En":
+            idioma = "configEN.json"
+            break;
+        case "Pt":
+            idioma = "configPT.json"
+            break;
+        default:
+            idioma = "configES.json"
+            break;
+    }
+    const filePath = "/reto5/" + idPerfil + "/perfil.json";
     async function obtenerDatos() {
         const response = await fetch(filePath);
         const data = await response.json();
         const dataArray = Object.values(data);
-        return dataArray;
+
+        const respuesta = await fetch("/reto5/conf/" + idioma);
+        const data1 = await respuesta.json();
+        const datos = Object.values(data1);
+        return [dataArray, datos];
     }
-    
-    obtenerDatos().then(data => {
+
+    obtenerDatos().then(([data, dato]) => {
         document.getElementById("foto-perfil")
-            .src = "/reto5/"+ idPerfil+ "/" + data[11];
+            .src = "/reto5/" + idPerfil + "/" + data[11];
         document.getElementById("informacion-perfil")
             .firstChild.textContent = data[0];
         document.getElementById("descripcion-persona")
@@ -17,21 +38,23 @@ function iniciar(idPerfil) {
         let lF = document.getElementsByClassName("datos-favoritos")
             .item(0);
         for (let i = 0; i < 5; i++) {
-            let textoLi = lF.children[i].textContent;
-            lF.children[i].innerHTML = textoLi + data[i + 2];
+            lF.children[i].innerHTML = dato[i + 6] + data[i + 2];
         }
-        caja=document.getElementById("caja").children[3].children[0]
-        .innerHTML=data[7];
+        const regex = "[email]";
+        const enlace = '<a href=https://www.example.com>' + data[7] + "</a>";
+        const textoNuevo = dato[11].replace(regex, enlace);
+        document.getElementById("caja").children[3].innerHTML = textoNuevo;
     })
         .catch(error => {
             console.error("Error obteniendo datos:", error);
         });
-    
+
 }
 
 const urlParams = new URLSearchParams(window.location.search);
 const idPerfil = urlParams.get('id'); // Obtener el parámetro "id" de la URL
+const idioma = urlParams.get('idioma');
 
-window.addEventListener("DOMContentLoaded", iniciar(idPerfil));
+window.addEventListener("DOMContentLoaded", iniciar(idPerfil, idioma));
 
 
--- js/index1.js
+++ js/index1.js
@@ -1,29 +1,55 @@
-function iniciar() {
+function iniciar(idioma) {
+    switch (idioma) {
+        case "Es":
+            idioma = "configES.json"
+            break;
+        case "En":
+            idioma = "configEN.json"
+            break;
+        case "Pt":
+            idioma = "configPT.json"
+            break;
+        default:
+            idioma = "configES.json"
+            break;
+    }
     const filePath = '/reto5/datos/index.json';
     async function obtenerDatos() {
         const response = await fetch(filePath);
         const data = await response.json();
         const dataArray = Object.values(data);
-        return dataArray;
+
+        const respuesta = await fetch("/reto5/conf/" + idioma);
+        const data1 = await respuesta.json();
+        const datos = Object.values(data1);
+        return [dataArray, datos];
     }
 
-    obtenerDatos().then(data => {
-       let cantidadElementos = data.length;
-        let contadorPrincipal=0;
+    obtenerDatos().then(([data, dato]) => {
+        const header=document.getElementById("uno");
+        header.children[0].innerHTML=dato[0][0];
+        header.children[1].innerHTML=dato[0][1];
+        header.children[2].innerHTML=dato[0][2];
+        const h=document.getElementById("nombre-usuario");
+        h.innerHTML=dato[13]+", "+data[0].nombre;
+        document.getElementById("boton").innerHTML=dato[12];
+        document.getElementById("entrada").placeholder=dato[4]+"...";
+        let cantidadElementos = data.length;
+        let contadorPrincipal = 0;
         while (cantidadElementos > 0) {
             let contPrinpal = document.getElementById("contenedor-principal");
             let elementoFila = document.createElement("div");
             elementoFila.classList.add("fila");
             contPrinpal.appendChild(elementoFila);
-            let contador=0;
-            while (cantidadElementos>0 && contador<4) {
+            let contador = 0;
+            while (cantidadElementos > 0 && contador < 4) {
                 elementoCelda = document.createElement("div");
                 elementoCelda.classList.add("contenedor")
                 elementoImg = document.createElement("img")
-                elementoImg.src = "reto5/"+data[contadorPrincipal].imagen;
-                elementoImg.alt="Imagen ";
-                elementoPar=document.createElement("p");
-                elementoPar.textContent=data[contadorPrincipal].nombre;
+                elementoImg.src = "reto5/" + data[contadorPrincipal].imagen;
+                elementoImg.alt = "Imagen ";
+                elementoPar = document.createElement("p");
+                elementoPar.textContent = data[contadorPrincipal].nombre;
                 elementoCelda.appendChild(elementoImg);
                 elementoCelda.appendChild(elementoPar);
                 elementoFila.appendChild(elementoCelda);
@@ -33,11 +59,16 @@ function iniciar() {
                 cantidadElementos--;
             }
         }
+        const pp=document.getElementsByTagName("footer").item(0)
+        .children[0].innerHTML=dato[3];
+        console.log(pp);
     })
         .catch(error => {
             console.error("Error obteniendo datos:", error);
         });
 
 }
+const urlParams = new URLSearchParams(window.location.search);
+const idioma = urlParams.get('idioma');
 
-window.addEventListener("DOMContentLoaded", iniciar);
+window.addEventListener("DOMContentLoaded", iniciar(idioma));
